46c33fe34f3ac43c97b903a3701c5e659cfb6af5
Rodrigo Igor Villanueva Nieto <rodrigo.villanueva.c@senasica.gob.mx>
2018-10-26 12:29:08 -0500

    Cliente HTTP inicio

---
+++ resources/views/principal.blade.php
@@ -0,0 +1,23 @@
+@extends('master')
+
+@section('content')
+
+
+
+
+<div class="list-group">
+        <div class="list-group">
+            <a href="#" class="list-group-item ">Obtener todos los Estudiantes</a>
+            <a href="#" class="list-group-item ">Obtener todos los Profesores</a>
+            <a href="#" class="list-group-item ">Obtener todos los Cursos</a>
+        </div>
+
+
+        <div class="list-group">
+                <a href="#" class="list-group-item ">Obtener Un Estudiante</a>
+                <a href="#" class="list-group-item ">Obtener Un Profesor</a>
+                <a href="#" class="list-group-item ">Obtener Un Curso</a>
+            </div>
+
+    </div>
+@endsection
